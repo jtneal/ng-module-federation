a9fc540d5f9b1a4abd7385cbcbf8eba34aea5007
Jason Neal <jasonneal1@quickenloans.com>
2021-03-27 19:45:52 -0400

    Refactor using cleaner approach

--- projects/library/schematics/ng-add/index.ts
+++ projects/library/schematics/ng-add/index.ts
@@ -1,10 +1,12 @@
-import { join, normalize, strings } from '@angular-devkit/core';
+import { normalize, strings } from '@angular-devkit/core';
 import { apply, applyTemplates, chain, externalSchematic, mergeWith, move, Rule, SchematicsException, Tree, url } from '@angular-devkit/schematics';
 import { getWorkspace, updateWorkspace } from '@schematics/angular/utility/workspace';
 import { ProjectType } from '@schematics/angular/utility/workspace-models';
 
 import { getSupportedApplications } from '../utility/applications';
 import { updateBootstrap } from '../utility/bootstrap';
+import { chainIf } from '../utility/chain';
+import { updateAngularConfig } from '../utility/config';
 import { addRequiredDependencies, installDependencies, optInToWebpack5 } from '../utility/dependencies';
 import { addMicroFrontendConfiguration, generateMicroFrontendEntryPoint } from '../utility/micro-frontend';
 import { addNavToShell } from '../utility/nav';
@@ -34,70 +36,10 @@ export function ngAdd(options: Schema): Rule {
       throw new SchematicsException(`Only projects of type "${ProjectType.Application}" are supported!`);
     }
 
-    const port = parseInt(options.port, 10);
-
-    if (isNaN(port)) {
-      throw new SchematicsException('Port must be a number!');
-    }
-
-    const build = config.targets.get('build');
-
-    if (!build) {
-      throw new SchematicsException('Invalid application configuration: "build" not found!');
-    }
-
-    if (!build.options) {
-      build.options = { };
-    }
-
-    if (!build.configurations) {
-      build.configurations = { production: { } };
-    }
-
-    if (!build.configurations.production) {
-      build.configurations.production = { };
-    }
-
-    const serve = config.targets.get('serve');
-
-    if (!serve) {
-      throw new SchematicsException('Invalid application configuration: "serve" not found!');
-    }
-
-    if (!serve.options) {
-      serve.options = { };
-    }
-
-    if (!serve.configurations) {
-      serve.configurations = { production: { } };
-    }
-
-    if (!serve.configurations.production) {
-      serve.configurations.production = { };
-    }
-
-    const test = config.targets.get('test');
-
-    if (!test) {
-      throw new SchematicsException('Invalid application configuration: "test" not found!');
-    }
-
-    if (!test.options) {
-      test.options = { };
-    }
-
-    const webpackPath = join(normalize(config.root), 'webpack.config.js');
-    const webpackProdPath = join(normalize(config.root), 'webpack.prod.config.js');
-
-    build.options.extraWebpackConfig = webpackPath;
-    build.configurations.production.extraWebpackConfig = webpackProdPath;
-    serve.options.extraWebpackConfig = webpackPath;
-    serve.options.port = port;
-    serve.configurations.production.extraWebpackConfig = webpackProdPath;
-    test.options.extraWebpackConfig = webpackPath;
-
     const applications = getSupportedApplications(workspace, project);
+    const build = config.targets.get('build');
     const isShell = options.type === 'shell';
+    const port = parseInt(options.port, 10);
     const sourceRoot = config.sourceRoot as string;
 
     const templateSource = apply(url('./files'), [
@@ -105,32 +47,26 @@ export function ngAdd(options: Schema): Rule {
       move(normalize(config.root)),
     ]);
 
-    const shellRules: Rule[] = [
-      addMicroFrontendConfiguration(applications, sourceRoot),
-      optInToWebpack5(),
-      addRequiredDependencies(applications, project),
-      installDependencies(),
-    ];
-    const microRules: Rule[] = [];
-
-    if (!options.minimal) {
-      shellRules.push(
-        addNavToShell(applications, sourceRoot, project),
-        addRouteToShell(sourceRoot),
-      );
-
-      microRules.push(
-        generateMicroFrontendEntryPoint(project, config.prefix as string),
-        addRouteToMicroFrontend(sourceRoot, project),
-      );
-    }
-
     return chain([
+      updateAngularConfig(config, port),
       updateWorkspace(workspace),
       mergeWith(templateSource),
-      updateBootstrap(isShell, build.options.main as string),
+      updateBootstrap(isShell, build?.options?.main as string),
       externalSchematic('ngx-build-plus', 'ng-add', { project: options.project }),
-      chain(isShell ? shellRules : microRules),
+      chainIf(isShell, [
+        addMicroFrontendConfiguration(applications, sourceRoot),
+        optInToWebpack5(),
+        addRequiredDependencies(applications, project),
+        installDependencies(),
+        chainIf(!options.minimal, [
+          addNavToShell(applications, sourceRoot, project),
+          addRouteToShell(sourceRoot),
+        ]),
+      ]),
+      chainIf(!isShell && !options.minimal, [
+        generateMicroFrontendEntryPoint(project, config.prefix as string),
+        addRouteToMicroFrontend(sourceRoot, project),
+      ]),
     ]);
   };
 }
--- projects/library/schematics/utility/routes.ts
+++ projects/library/schematics/utility/routes.ts
@@ -1,10 +1,11 @@
 import { strings, normalize } from '@angular-devkit/core';
 import { Rule, Tree } from '@angular-devkit/schematics';
-import { addImportToModule, addRouteDeclarationToModule } from '@schematics/angular/utility/ast-utils';
+import { addRouteDeclarationToModule, getSourceNodes, insertImport } from '@schematics/angular/utility/ast-utils';
 import { InsertChange } from '@schematics/angular/utility/change';
 import { ROUTING_MODULE_EXT } from '@schematics/angular/utility/find-module';
 import * as ts from 'typescript';
 
+import { insertAfterImports } from './ast';
 import { handleChanges } from './changes';
 
 export function addRouteToMicroFrontend(sourceRoot: string, projectName: string): Rule {
@@ -13,13 +14,13 @@ export function addRouteToMicroFrontend(sourceRoot: string, projectName: string)
     const classified = strings.classify(`${projectName}Component`);
     const modulePath = normalize(`${sourceRoot}/app/${dasherized}/${dasherized}${ROUTING_MODULE_EXT}`);
     const source = ts.createSourceFile(modulePath, host.read(modulePath)?.toString() || '', ts.ScriptTarget.Latest, true);
-    const changes = [addImportToModule(source, modulePath, classified, `./${dasherized}.component`)[1]];
-
-    changes.push(addRouteDeclarationToModule(
-      source,
-      modulePath,
-      `{ component: ${classified}, path: '' }`,
-    ) as InsertChange);
+    const nodes = getSourceNodes(source);
+    const route = `{ children: mfeRoutes, component: ${classified}, path: '' }`;
+    const changes = [
+      insertImport(source, modulePath, classified, `./${dasherized}.component`),
+      insertAfterImports(nodes, modulePath, `const mfeRoutes: Routes = [];`),
+      addRouteDeclarationToModule(source, modulePath, route) as InsertChange,
+    ];
 
     return handleChanges(host, modulePath, changes);
   };
@@ -32,8 +33,8 @@ export function addRouteToShell(sourceRoot: string): Rule {
     const routes = '...Object.keys(microFrontends).map((m) => createMicroFrontendDynamicRoute(microFrontends[m])),';
 
     const changes = [
-      addImportToModule(source, modulePath, 'createMicroFrontendDynamicRoute', 'ng-module-federation')[1],
-      addImportToModule(source, modulePath, 'microFrontends', '../micro-frontends')[1],
+      insertImport(source, modulePath, 'createMicroFrontendDynamicRoute', 'ng-module-federation'),
+      insertImport(source, modulePath, 'microFrontends', '../micro-frontends'),
       addRouteDeclarationToModule(source, modulePath, routes) as InsertChange,
     ];
 
